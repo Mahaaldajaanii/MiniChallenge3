11d3bf4cd7350c1bb2d151ba811c520a42245ebf
ShahadAlkamli <shahad.s.alkamli@hotmail.com>
2022-02-23 11:49:07 +0300

    s

---
+++ MiniChallenge3/homeVC.swift
@@ -0,0 +1,29 @@
+//
+//  homeVC.swift
+//  MiniChallenge3
+//
+//  Created by Shahad Alkamli on 23/02/2022.
+//
+
+import UIKit
+
+class homeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
